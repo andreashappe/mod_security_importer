5aeabc7ff363700970a6451a2602a26c0b457bde
Andreas Happe <andreashappe@snikt.net>
2015-08-07 14:56:57 +0200

    analyze_simple: can now sort by source|destination|timestamp

--- log_importer/log_analyzer/overview.py
+++ log_importer/log_analyzer/overview.py
@@ -2,7 +2,7 @@
 
 import argparse
 from log_importer.data.db_helper import setup_connection
-from log_importer.data.objects import Incident
+from log_importer.data.objects import Incident, Source, Destination
 
 def output_details(incident, detail):
     """ outputs a single line (describing one incident) """
@@ -19,13 +19,28 @@ def output_overview():
                         description="Give an high-level overview of database.")
     parser.add_argument('database', help="Database to analyze")
 
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-t", "--sort-by-time", action="store_true")
+    group.add_argument("-s", "--sort-by-source-ip", action="store_true")
+    group.add_argument("-d", "--sort-by-destination-ip", action="store_true")
+
     args = parser.parse_args()
 
     # open database
     session = setup_connection(create_db=False, path=args.database)
 
+    # get results
+    results = session.query(Incident)
+
+    if args.sort_by_source_ip:
+        results = results.join(Incident.source).order_by(Source.ip, Source.port).all()
+    elif args.sort_by_destination_ip:
+        results = results.join(Incident.destination).order_by(Destination.ip, Destination.port).all()
+    else:
+        results = results.order_by(Incident.timestamp).all()
+
     # show all incidences
-    for incident in session.query(Incident).all():
+    for incident in results:
         for detail in incident.details:
             output_details(incident, detail)
 
