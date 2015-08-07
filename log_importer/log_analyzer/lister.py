4f83f619adcaea7d3601ba9bb5d00f20aff4bc18
Andreas Happe <andreashappe@snikt.net>
2015-08-07 15:45:11 +0200

    list_incident_types: show a list with all incident types
    The type id might be used in future analyze tools as command line
    parameter.

---
+++ log_importer/log_analyzer/lister.py
@@ -0,0 +1,30 @@
+import argparse
+
+from sqlalchemy import func, desc
+from log_importer.data.db_helper import setup_connection
+from log_importer.data.objects import Destination, IncidentCatalogEntry,\
+                                      IncidentDetail, Incident
+
+def retrieve_data(session):
+    """ gets raw output data from the database """
+    return session.query(IncidentCatalogEntry).all()
+
+
+def output_incident_types():
+    """ group database by <ip, port and error-message>
+        and output group error counts. """
+
+    parser = argparse.ArgumentParser(\
+                    description="List all known incident types.")
+    parser.add_argument('database', help="Database to analyze")
+    args = parser.parse_args()
+
+    # open database
+    session = setup_connection(create_db=False, path=args.database)
+
+    # show the different catalog types
+    for i in retrieve_data(session):
+        print("id %3d: %s:%d %s" % (i.catalog_id, i.config_file, i.config_line, i.message))
+
+    # close database
+    session.close()
