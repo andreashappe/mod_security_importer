60b413cee767b076dfe1c5665a5347699a7159f6
Andreas Happe <andreashappe@snikt.net>
2016-02-03 00:35:51 +0100

    prepare multiprocessing (thanks to https://github.com/FireFart)
    Longterm goal is to switch to a parallel input-processing action.
    
    This is the first step where we introduce the multiprocessing
    architecture but do not activate it --- we do not have a database
    driver supporting parallel insert in-place yet.

--- log_importer/importer.py
+++ log_importer/importer.py
@@ -4,6 +4,24 @@ from log_importer.log_import.parser import parse_incident
 from log_importer.log_import.reader import read_from_file
 from log_importer.data.db_helper import setup_connection
 
+from multiprocessing import Process, cpu_count, Queue
+
+
+def import_worker(jobs, session, import_parts):
+
+    f = jobs.get()
+    while f is not None:
+        print("parsing " + f.name)
+        tmp = read_from_file(f)
+
+        incident = parse_incident(session, tmp[0], tmp[1], include_parts=args.import_parts)
+
+        print("adding " + f.name + " to db")
+        session.add(incident)
+        session.commit()
+
+	f = jobs.get()
+
 
 def import_log_to_database():
     parser = argparse.ArgumentParser(description="Import Log-Files into Database.")
@@ -21,15 +39,27 @@ def import_log_to_database():
     # open database
     session = setup_connection(create_db=True, path=args.database)
 
+    # WIP: add a minimal multiprocessing implementation
+    jobs = Queue()
+    workers = []
+    num_workers = 1 # TODO: allow for multiple workers (db-depenedent)
+
+    for w in range(num_workers):
+        p = Process(target=import_worker, args=(jobs, session, args.import_parts))
+        p.start()
+        workers.append(p)
+
     # add files
     for f in args.files:
-        print("parsing " + f.name)
-        tmp = read_from_file(f)
-        incident = parse_incident(session, tmp[0], tmp[1], include_parts=args.import_parts)
+        jobs.put(f)
 
-        print("adding " + f.name + " to db")
-        session.add(incident)
-        session.commit()
+    # add stop bit
+    for i in range(num_workers):
+        jobs.put(None)
+
+    # wait for workers to finish
+    for p in workers:
+        p.join()
 
     # close database
     session.close()
