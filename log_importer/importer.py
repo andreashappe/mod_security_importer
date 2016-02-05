c8ab84bf321bf621fc7047a12793022c3ff044f7
Andreas Happe <andreashappe@snikt.net>
2016-02-05 18:03:50 +0100

    add some classes and reduce global variables. by a lot

--- log_importer/importer.py
+++ log_importer/importer.py
@@ -6,122 +6,54 @@ from log_importer.log_import.parser import parse_incident
 from log_importer.log_import.reader import read_from_file
 from log_importer.data.db_helper import setup_connection
 from log_importer.data.objects import *
+from log_importer.cache import SourceCache, DestinationCache, IncidentDetailCache
 from datetime import datetime
 from concurrent import futures
 
 from multiprocessing import Pool, cpu_count
 
+class IncidentCount:
+    def __init__(self, session):
+        self.counter = retrieve_new_id_for(session, Incident)
 
-counter = 0
+    def getNext(self):
+        self.counter += 1
+        return self.counter
+
+class IncidentCache:
+    def __init__(self):
+        self.incidentCache = []
+        self.partCache = []
+
+    def addIncident(self, incident):
+        self.incidentCache.append(incident)
+
+    def addPart(self, part):
+        self.partCache.append(part)
+
+    def getCount(self):
+        return len(self.incidentCache)
+
+    def writeParts(self, conn):
+        if self.partCache:
+            conn.execute(Part.__table__.insert().values(self.partCache))
+        self.partCache = []
+
+    def writeIncidents(self, conn):
+        if self.incidentCache:
+            conn.execute(Incident.__table__.insert().values(self.incidentCache))
+        self.incidentCache = []
+
 last = datetime.now()
-internal_id = 0
 
-class SourceCache:
-
-    tmp_id = None
-    session = None
-    journal = []
-
-    def __init__(self, session):
-        self.session = session
-        self.tmp_id = retrieve_new_id_for(session, Source)
-
-    def get_id(self, ip, port):
-        tmp = self.session.query(Source.id).filter_by(ip=ip).filter_by(port=port).first()
-        if tmp is None:
-            self.tmp_id += 1
-            self.journal.append({'id': self.tmp_id, 'ip': ip, 'port': port})
-            return self.tmp_id
-        else:
-            return tmp[0]
-
-    def sync_to_db(self, conn):
-        if self.journal:
-            conn.execute(Source.__table__.insert().values(self.journal))
-        self.journal = []
-
-class DestinationCache:
-    tmp_id = None
-    cache = {}
-    session = None
-    journal = []
-
-    def __init__(self, session):
-        self.session = session
-        self.tmp_id = retrieve_new_id_for(session, Destination)
-
-    def get_id(self, ip, port):
-        key = ip + "-" + str(port)
-        if key not in self.cache:
-            tmp = self.session.query(Destination.id).filter_by(ip=ip).filter_by(port=port).first()
-            if tmp is None:
-                self.tmp_id += 1
-                self.journal.append({'id': self.tmp_id, 'ip': ip, 'port': port})
-                tmp = self.tmp_id 
-            else:
-                tmp = tmp[0]
-            self.cache[key] = tmp
-        return self.cache[key]
-
-    def sync_to_db(self, conn):
-        if self.journal:
-            conn.execute(Destination.__table__.insert().values(self.journal))
-        self.journal = []
-
-
-class IncidentDetailCache:
-    tmp_id = None
-    cache = {}
-    session = None
-    journal = []
-    journal_details = []
-
-    def __init__(self, session):
-        self.session = session
-        self.tmp_id = retrieve_new_id_for(session, Destination)
-
-    def add_detail(self, incident_id, message, cfile, key, line):
-        if key not in self.cache:
-            tmp = self.session.query(IncidentCatalogEntry.id).filter_by(catalog_id=key).first()
-            if tmp is None:
-                self.tmp_id += 1
-                self.journal.append({'id': self.tmp_id, 'message': message, 'config_file': cfile, 'catalog_id': key, 'config_line': line})
-                tmp = self.tmp_id 
-            else:
-                tmp = tmp[0]
-            self.cache[key] = tmp
-        ice_id = self.cache[key]
-        self.journal_details.append({'incident_id': incident_id, 'incident_catalog_id': ice_id})
-
-    def sync_to_db(self, conn):
-        if self.journal:
-            conn.execute(IncidentCatalogEntry.__table__.insert().values(self.journal))
-        self.journal = []
-
-        if self.journal_details:
-            conn.execute(IncidentDetail.__table__.insert().values(self.journal_details))
-        self.journal_details = []
-
-
-incident_categories = []
-incidents=[]
-
-cache_destination = None
-cache_source = None
-cache_details = None
-
-def forward_to_db(session, i):
-    global counter
+def forward_to_db(session, i, id_counter, incident_cache, cache_destination, cache_source, cache_details, diff=1000):
     global last
-    global internal_id
 
-    global incident_categories, incidents
-    global cache_destination, cache_source, cache_details
-
+    internal_id = id_counter.getNext()
     source_id = cache_source.get_id(i['source'][0], i['source'][1])
     destination_id = cache_destination.get_id(i['destination'][0], i['destination'][1])
 
-    incidents.append({ 'id': internal_id,
+    incident = { 'id': internal_id,
                        'fragment_id': i['fragment_id'],
                        'timestamp': i['timestamp'],
                        'unique_id': i['unique_id'],
@@ -131,37 +63,30 @@ def forward_to_db(session, i):
                        'method': i['method'],
                        'path': i['path'],
                        'http_code': i['http_code']
-    })
+    }
+
+    incident_cache.addIncident(incident)
 
     for p in i['parts']:
-        p['id'] = internal_id
-        incident_categories += p
+        p['incident_id'] = internal_id
+        incident_cache.addPart(p)
 
     [ cache_details.add_detail(internal_id, p['msg'], p['file'], int(p['id']), int(p['line'])) for p in i['details'] if 'msg' in p.keys()]
 
-    diff = 1000
-    counter += 1
-
-    if counter % diff == 0:
+    if incident_cache.getCount() % diff == 0:
         conn = session.connection()
         cache_source.sync_to_db(conn)
         cache_destination.sync_to_db(conn)
-        if incident_categories:
-            conn.execute(Part.__table__.insert().values(incident_categories))
-        if incidents:
-            conn.execute(Incident.__table__.insert().values(incidents))
+
+        incident_cache.writeIncidents(conn)
+        incident_cache.writeParts(conn)
         cache_details.sync_to_db(conn)
 
-        incident_categories = []
-        incidents = []
-
     	session.commit()
-	tmp = (datetime.now() - last).total_seconds()/diff*1000.0
-        print "counter " + str(counter) + ": " + str(tmp) + "ms/import"
-	last = datetime.now()
-
-    internal_id += 1
-
+        tmp = (datetime.now() - last).total_seconds()/diff*1000.0
+        print("timing: : " + str(tmp) + "ms/import")
+        last = datetime.now()
+    return incident
 
 def retrieve_new_id_for(session, klass):
     tmp = session.query(sqlalchemy.func.max(klass.id)).first()[0]
@@ -186,34 +111,26 @@ def import_log_to_database():
         print("not adding parts")
 
     # open database to calculate num_worker
-    global internal_id, incidents, incident_categories
-    global cache_destination, cache_source, cache_details
-
     session = setup_connection(create_db=True, path=args.database)
 
     cache_destination = DestinationCache(session)
     cache_source = SourceCache(session)
     cache_details = IncidentDetailCache(session)
+    incident_counter = IncidentCount(session)
+    incident_cache = IncidentCache()
 
-    internal_id = retrieve_new_id_for(session, Incident)
-
-    files = [args.files[0].name]*20000
+    # files = [args.files[0].name]*20000
+    files = [ f.name for f in args.files ]
     with futures.ProcessPoolExecutor(max_workers=max(1, cpu_count()-1)) as executor:
         for incident in executor.map(tmp, files):
-            forward_to_db(session, incident)
+            forward_to_db(session, incident, incident_counter, incident_cache, cache_destination, cache_source, cache_details)
 
     # close database
     conn = session.connection()
     cache_source.sync_to_db(conn)
     cache_destination.sync_to_db(conn)
-    if incident_categories:
-        conn.execute(Part.__table__.insert().values(incident_categories))
-    if incidents:
-        conn.execute(Incident.__table__.insert().values(incidents))
+    incident_cache.writeIncidents(conn)
+    incident_cache.writeParts(conn)
     cache_details.sync_to_db(conn)
-
-    incident_categories = []
-    incidents = []
-
     session.commit()
     session.close()
--- log_importer/tests/db_import_test.py
+++ log_importer/tests/db_import_test.py
@@ -1,18 +1,25 @@
 """ Tests data serialization of objects into the database yields
     uncorrupted data. """
 
-import datetime
+import sqlalchemy
+
+from datetime import datetime
 
 from log_importer.log_import.parser import parse_incident
 from log_importer.log_import.reader import read_file
 from log_importer.data.db_helper import setup_connection
-from log_importer.data.objects import Incident
-from log_importer.importer import forward_to_db
+from log_importer.data.objects import Incident, Source
+from log_importer.importer import forward_to_db, IncidentCount, IncidentCache
+from log_importer.cache import SourceCache, DestinationCache, IncidentDetailCache
+
+
+counter = 0
+last = datetime.now()
 
 def common_data(i, incident):
     """ common tests that should hold in all cases. """
     assert i.fragment_id == '7cf8df3f'
-    assert i.timestamp == datetime.datetime(2015, 3, 30, 21, 10, 38),\
+    assert i.timestamp == datetime(2015, 3, 30, 21, 10, 38),\
             "timestamp is: %r" % incident.timestamp # should be in UTC
     assert i.unique_id == u'VRm7zgr5AlMAAClwIZoAAAAU'
     assert i.source.ip == u'10.199.23.1'
@@ -35,11 +42,18 @@ def test_import_without_parts():
 
     result = read_file('log_importer/tests/test_files/file_read_test.txt')
     session = setup_connection(create_db=True)
+
+    cache_destination = DestinationCache(session)
+    cache_source = SourceCache(session)
+    cache_details = IncidentDetailCache(session)
+    incident_counter = IncidentCount(session)
+    incident_cache = IncidentCache()
+
     incident = parse_incident(result, include_parts=False)
-    incidentObject = forward_to_db(session, incident)
+    incidentObject = forward_to_db(session, incident, incident_counter, incident_cache, cache_destination, cache_source, cache_details, diff=1)
 
     # reload from db
-    i = session.query(Incident).filter(Incident.id == incidentObject.id).first()
+    i = session.query(Incident).filter(Incident.id == incidentObject['id']).first()
 
     common_data(i, incident)
     assert not i.parts
@@ -49,11 +63,18 @@ def test_import_with_parts():
 
     result = read_file('log_importer/tests/test_files/file_read_test.txt')
     session = setup_connection(create_db=True)
+
+    cache_destination = DestinationCache(session)
+    cache_source = SourceCache(session)
+    cache_details = IncidentDetailCache(session)
+    incident_counter = IncidentCount(session)
+    incident_cache = IncidentCache()
+
     incident = parse_incident(result, include_parts=True)
-    incidentObject = forward_to_db(session, incident)
+    incidentObject = forward_to_db(session, incident, incident_counter, incident_cache, cache_destination, cache_source, cache_details, diff=1)
 
     # reload from db
-    i = session.query(Incident).filter(Incident.id == incidentObject.id).first()
+    i = session.query(Incident).filter(Incident.id == incidentObject['id']).first()
 
     common_data(i, incident)
     assert len(i.parts) == 6
